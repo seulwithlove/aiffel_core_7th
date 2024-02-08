2eda413464f0872984d2dc03989d4d7e77223bf9
seulwithlove <love.and.seul@gmail.com>
2024-02-08 17:21:43 +0900

    📝 Submit Flutter Quest03

---
+++ Flutter/Quest03/quest03/android/app/src/main/kotlin/com/example/quest03/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.quest03
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Flutter/Quest03/quest03/lib/is_cat.dart
@@ -0,0 +1,5 @@
+/* OneScreen -> TwoScreen, TwoScreen -> OneScreen 전달할 데이터 형식 */
+class IsCat {
+  bool is_cat;
+  IsCat(this.is_cat);
+}
